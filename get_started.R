0714d85cddc9d93a7c918ed451d3b75d0813d4d7
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-05-03 02:28:01 +0100

    Upload Day 2 materials

---
+++ get_started.R
@@ -0,0 +1,7 @@
+# Sourcing this will make sure that you have the tidyverse and other key packages installed.
+# Note that I am using pacman rather than install.packages. This way, only packages
+# that are missing are installed; install.packages also performs updates.
+
+install.packages("pacman")
+
+pacman::p_load(tidyverse, lavaan, lme4, easystats, modelsummary)
